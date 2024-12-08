1c83edeb1cb454f1fb43dafac313b7a89aa4cdee
BugHunter-X <askzenai@gmail.com>
2024-12-08 21:12:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,12 @@
+```javascript
+// pages/index.js
+import { Fragment } from 'react';
+export default function Home() {
+  return (
+    <Fragment>
+      <h1>My Next.js App</h1>
+      <p>This is a test.</p>
+    </Fragment>
+  );
+}
+```
